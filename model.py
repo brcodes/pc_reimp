3d91ea40f49a276546442a2f5f5f3ec8b61ffb81
Kevin Brown <kevin.s.brown@uconn.edu>
2020-11-12 15:22:45 -0800

    Added some comments about current state of model parameters.

--- model.py
+++ model.py
@@ -4,22 +4,34 @@ import numpy as np
 
 class Model:
     def __init__(self, data, inpveclen=1, numclasses=10, numlayers=2, iteration=1):
+        # long term we will probably want to use a data "generator" which pulls batches
+        #    from a large set of patterns stored on disk, which helps with RAM.  I have
+        #    an example of how to do this from Nick in the EARSHOT project (I can help
+        #    with coding that also.) So you won't pass in "data", you'll pass in some object
+        #    that the model can call to get a training batch from.
         self.data = data
         self.iteration = iteration
+        # if we are passing in the data - or a location to it (like a generator that will pull samples from files),
+        #   you don't need to tell the model the inputsize - it can sniff it from one of the patterns
         self.inpveclen = inpveclen
+        # ditto with numclasses - just look at the length of any of the output vectors
         self.numclasses = numclasses
         self.numlayers = numlayers
         for i in range(1, (numlayers + 1)):
             self.layersize = 2 ** (2 * i + 3)
+        # this is a number (set of numbers), one per layer, assumed and not computed
         self.sigma = np.std((I - f(Ur)).T * (I - f(Ur)))
         self.alpha = " related to variance of Gaussian priors "
         self.lambda = " related to variance of Gaussian priors "
         self.U = np.random.rand()
         self.r = np.zeros()
-        self.k =
+        self.k = " this is the learning rate.  It could be a constant, or a function that depends on epoch. (We can talk about this.)"
 
 
 
+# this is a common trick that lets you combine "main loop stuff" with objects you
+#   can import later.
+if __name__ == '__main__':
 
     """ Inputs to the Model """
 
@@ -52,6 +64,8 @@ class Model:
     E = E1 + g(r) + h(U)
 
     # Prior distributions
+    # N.B. - we also need the derivative of any of the priors with respect to their
+    #   arguments
 
     def g(r):
         for i in range(0, numlayers + 1):
