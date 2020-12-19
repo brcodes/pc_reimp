6672aed3fdfb0525afbb53fa5cc7d1bc77efa494
Kevin Brown <kevin.s.brown@uconn.edu>
2020-12-18 16:03:58 -0800

    Data utilities and tests.
    +model passes all tests as of this commit

--- pypredcoding/data.py
+++ pypredcoding/data.py
@@ -1,23 +1,20 @@
 from keras.datasets import mnist
 from keras.utils import np_utils
-from numpy import ceil
+from numpy import ceil,prod
+
 
 def get_mnist_data(frac_samp=None,return_test=False):
     '''
-    Returns MNIST training examples (and potentially test) data.  Images
-    are flattened 28 x 28 images (784 pixels), and the target patterns are
-    one-hot vectors.
+    Returns MNIST training examples (and potentially test) data.  Images are
+    returned in an array of n_samples x 28 x 28, and target patterns are
+    n_samples x num_classes one hot vectors.
 
     keras's mnist load returns 60,000 training samples and 10,000 test samples.
     Set frac_samp to a number in [0,1] to reduce the proportion of samples
     returned.
     '''
+    # read from Keras
     (X_train,y_train),(X_test,y_test) = mnist.load_data()
-    # flattening of images into vectors of pixels
-    X_train = X_train.reshape(X_train.shape[0],X_train.shape[1]*X_train.shape[2])
-    X_test = X_test.reshape(X_test.shape[0],X_test.shape[1]*X_test.shape[2])
-    # scaling
-    # ?
     # conversion of target patterns to one-hot vectors
     y_test = np_utils.to_categorical(y_test)
     y_train = np_utils.to_categorical(y_train)
@@ -30,5 +27,36 @@ def get_mnist_data(frac_samp=None,return_test=False):
         n_test = X_test.shape[0]
     # send it all back
     if return_test:
-        return (X_train[:n_train,:],y_train[:n_train,:]),(X_test[:n_test,:],y_test[:n_test,:])
-    return X_train[:n_train,:],y_train[:n_train,:]
+        return (X_train[:n_train,:,:],y_train[:n_train,:]),(X_test[:n_test,:,:],y_test[:n_test,:])
+    return X_train[:n_train,:,:],y_train[:n_train,:]
+
+
+def flatten_images(image_array):
+    '''
+    Accepts an array of N x dim_x x dim_y images (N images each of dim_x x dim_y size)
+    and returns a 2D array of flattened images
+
+    This function will fail on a single image unless you give it an empty first
+    axis (np.newaxis,:,:).
+    '''
+    N = image_array.shape[0]
+    dim_x = image_array.shape[1]
+    dim_y = image_array.shape[1]
+    return image_array.reshape(N,dim_x*dim_y)
+
+
+def inflate_vectors(vector_array,shape_2d=None):
+    '''
+    Accepts an array of N x s flattened images (vectors) and returns an array of
+    N x shape_2d[0] x shape_2d[1] images.  If shape_2d is none, images are
+    assumed to be square.
+
+    This function will fail on a single input vector unless you give it an empty
+    first axis (np.newaxis,:)
+    '''
+    N = vector_array.shape[0]
+    if shape_2d == None:
+        sq = int(sqrt(vector_array.shape[1]))
+        shape_2d[0] = sq
+        shape_2d[1] = sq
+    return vector_array.reshape(N,shape_2d[0],shape_2d[1])
---
+++ pypredcoding/tests/test_datautil.py
@@ -0,0 +1,35 @@
+import unittest
+import numpy as np
+from pypredcoding.data import *
+
+
+class TestDataUtil(unittest.TestCase):
+
+    def setUp(self):
+        # grab a few MNIST images
+        self.X,self.y = get_mnist_data()
+
+
+    def test_mnist_read(self):
+        '''
+        Make sure that the MNIST read returns what we think it should,
+        size-wise.
+        '''
+        self.assertEqual(self.X.shape,(60000,28,28))
+        self.assertEqual(self.y.shape,(60000,10))
+
+
+    def test_flatten(self):
+        '''
+        Makes sure flattening works using MNIST images.
+        '''
+        flat_X = flatten_images(self.X)
+        self.assertEqual(flat_X.shape,(60000,28*28))
+
+
+    def test_inflate(self):
+        '''
+        Inflates the target patterns from shape (N,10) to (N,5,2).
+        '''
+        inf_y = inflate_vectors(self.y,shape_2d=(5,2))
+        self.assertEqual(inf_y.shape,(60000,5,2))
