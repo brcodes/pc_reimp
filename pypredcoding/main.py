987bb51cdd1a7dd44aa025282dd55c130fb468bb
Kevin Brown <kevin.s.brown@uconn.edu>
2021-02-16 14:50:03 -0800

    Change main loop

--- pypredcoding/main.py
+++ pypredcoding/main.py
@@ -1,26 +1,17 @@
-import data as data
-import parameters as parameters
-import model as model
-
-
-
-def main():
-
-    # load MNIST data (0.000166 * 60,000 = ~10 images)
-    X_train, y_train = data.get_mnist_data(frac_samp=0.000166,return_test=False)
-
-    # flatten training image array to N_patterns x 784
-    X_flat = data.flatten_images(X_train)
-
-    # train model
-    self.model.train(X_flat, y_train)
-
-
-
-
-
-
+from data import get_mnist_data,flatten_images
+from parameters import ModelParameters
+from model import PredictiveCodingClassifier
 
 
 if __name__ == '__main__':
-    main()
+    # create and modify model parameters
+    p = ModelParameters()
+    # load data
+    X_train, y_train = get_mnist_data(frac_samp=0.000166,return_test=False)
+    # do any rescaling, normalization here
+    # flatten
+    X_flat = flatten_images(X_train)
+    # instantiate model
+    pcmod = PredictiveCodingClassifier(p)
+    # train
+    pcmod.train(X_flat,y_train)
