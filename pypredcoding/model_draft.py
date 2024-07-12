811829f81ab31f9e34998f2600236382628d96c7
Bryce <Rogers>
2024-07-12 15:55:55 -0400

    spcc fix: e_layer resets each layer, added to etot.

--- pypredcoding/model_draft.py
+++ pypredcoding/model_draft.py
@@ -168,6 +168,7 @@ class StaticPredictiveCodingClassifier:
         # We want to track E for Layer 1, Layer 2, Layer 3 (Li 212)
         # this loop will only tackle layer 1 and 2 in a 3 layer model.
         for i in range(1,len(self.r)):
+            E_layer = 0
             # Bottom up reconstruction error term, a vector
             bu_err = self.r[i-1] - self.f(self.U[i].dot(self.r[i]))[0]
         
@@ -182,15 +183,17 @@ class StaticPredictiveCodingClassifier:
             tot_err_sq = bu_err_sq + td_err_sq
             
             # Representation cost E for this layer, is comprised of a bu and td component. (it contains this form for all n-1 layers)
-            E_tot = E_tot + ((1 / self.p.sigma_sq[i]) * bu_err_sq) + ((1 / self.p.sigma_sq[i+1]) * td_err_sq)
-            E_tot = E_tot + self.h(self.U[i],self.p.lam[i])[0] + self.g(np.squeeze(self.r[i]),self.p.alpha[i])[0]
+            E_layer = E_layer + ((1 / self.p.sigma_sq[i]) * bu_err_sq) + ((1 / self.p.sigma_sq[i+1]) * td_err_sq)
+            E_layer = E_layer + self.h(self.U[i],self.p.lam[i])[0] + self.g(np.squeeze(self.r[i]),self.p.alpha[i])[0]
             # priors^^^
             '''
             check out sizing of Ui for h later
             '''
-            
             # Store
-            E_list.append(E_tot)
+            E_list.append(E_layer)
+            
+            # Add layer E to tot E
+            E_tot = E_tot + E_layer
             
             # Also calulate bottom up, top down, and total prediction error (ie. L2 norm of the error vector) for each layer
             PE_tot = np.sqrt(tot_err_sq)
@@ -203,6 +206,7 @@ class StaticPredictiveCodingClassifier:
         # ie the top layer, the localist layer
         # Bottom up reconstruction error term, a vector
         
+        E_layer = 0
         n = self.num_nonin_lyrs
         
         # C1 top layer cost term
@@ -217,11 +221,14 @@ class StaticPredictiveCodingClassifier:
         tot_err_sq = bu_err_sq + td_err_sq
         
         # Representation cost E for this layer, is comprised of a bu and td component. (it contains this form for all n-1 layers)
-        E_tot = E_tot + ((1 / self.p.sigma_sq[n]) * bu_err_sq) + ((1 / self.p.sigma_sq[n+1]) * td_err_sq)
-        E_tot = E_tot + self.h(self.U[n],self.p.lam[n])[0] + self.g(np.squeeze(self.r[n]),self.p.alpha[n])[0]
+        E_layer = E_layer + ((1 / self.p.sigma_sq[n]) * bu_err_sq) + ((1 / self.p.sigma_sq[n+1]) * td_err_sq)
+        E_layer = E_layer + self.h(self.U[n],self.p.lam[n])[0] + self.g(np.squeeze(self.r[n]),self.p.alpha[n])[0]
         # priors^^^
         # Store
-        E_list.append(E_tot)
+        E_list.append(E_layer)
+        
+        # Add layer E to tot E
+        E_tot = E_tot + E_layer
         
         # Also calulate bottom up, top down, and total prediction error (ie. L2 norm of the error vector) for each layer
         PE_tot = np.sqrt(tot_err_sq)
--- pypredcoding/non_tiled_updates_dot_only.py
+++ pypredcoding/non_tiled_updates_dot_only.py
@@ -86,6 +86,8 @@ for iteration in range(0,self.num_rUsimul_iters):
                                 
 '''
 separate e, pe calcs for plotting
+
+old way
 '''
 
 
@@ -111,6 +113,101 @@ def rep_cost(self):
         E = E + (self.h(self.U[i],self.p.lam[i])[0] + self.g(np.squeeze(self.r[i]),self.p.alpha[i])[0])
 
     return (E, PE_list)
+
+'''
+separate e, pe calcs for plotting
+
+new way (2024, July 12)
+
+math fixed (includes a bottom up term now)
+
+also stores Etot by layer (wrongly)
+fix so it does Etot and Elayer
+'''
+
+def rep_cost(self, label):
+    '''
+    Uses current r/U states to compute the least squares portion of the error
+    (concerned with accurate reconstruction of the input).
+    
+    this is called once per image in the training loop
+    '''
+    
+    # squared, sigma-weighted reconstruction error, with priors added below
+    E_tot = 0
+    E_list = []
+    # non squared, non weighted reconstruction error
+    PE_list = []
+
+    # We want to track E for Layer 1, Layer 2, Layer 3 (Li 212)
+    # this loop will only tackle layer 1 and 2 in a 3 layer model.
+    for i in range(1,len(self.r)):
+        # Bottom up reconstruction error term, a vector
+        bu_err = self.r[i-1] - self.f(self.U[i].dot(self.r[i]))[0]
+    
+        # Top down reconstruction error term, a vector
+        td_err = self.r[i] - self.f(self.U[i+1].dot(self.r[i+1]))[0]
+        
+        # Bottom up error term squared, a scalar
+        bu_err_sq = bu_err.T.dot(bu_err)
+        # Top down error term squared, also a scalar
+        td_err_sq = td_err.T.dot(td_err)
+        # Total
+        tot_err_sq = bu_err_sq + td_err_sq
+        
+        # Representation cost E for this layer, is comprised of a bu and td component. (it contains this form for all n-1 layers)
+        E_tot = E_tot + ((1 / self.p.sigma_sq[i]) * bu_err_sq) + ((1 / self.p.sigma_sq[i+1]) * td_err_sq)
+        E_tot = E_tot + self.h(self.U[i],self.p.lam[i])[0] + self.g(np.squeeze(self.r[i]),self.p.alpha[i])[0]
+        # priors^^^
+        '''
+        check out sizing of Ui for h later
+        '''
+        
+        # Store
+        E_list.append(E_tot)
+        
+        # Also calulate bottom up, top down, and total prediction error (ie. L2 norm of the error vector) for each layer
+        PE_tot = np.sqrt(tot_err_sq)
+        PE_bu = np.sqrt(bu_err_sq)
+        PE_td = np.sqrt(td_err_sq)
+        # Store
+        PE_list.append((PE_tot, PE_bu, PE_td))
+        
+    # Li 212 Layer 3
+    # ie the top layer, the localist layer
+    # Bottom up reconstruction error term, a vector
+    
+    n = self.num_nonin_lyrs
+    
+    # C1 top layer cost term
+    bu_err = self.r[n-1] - self.f(self.U[n].dot(self.r[n]))[0]
+    td_err = softmax(self.r[n]) - label[:,None] # Difference in order is because not a derivative
+    
+    # Bottom up error term squared, a scalar
+    bu_err_sq = bu_err.T.dot(bu_err)
+    # Top down error term squared, also a scalar
+    td_err_sq = td_err.T.dot(td_err)
+    # Total
+    tot_err_sq = bu_err_sq + td_err_sq
+    
+    # Representation cost E for this layer, is comprised of a bu and td component. (it contains this form for all n-1 layers)
+    E_tot = E_tot + ((1 / self.p.sigma_sq[n]) * bu_err_sq) + ((1 / self.p.sigma_sq[n+1]) * td_err_sq)
+    E_tot = E_tot + self.h(self.U[n],self.p.lam[n])[0] + self.g(np.squeeze(self.r[n]),self.p.alpha[n])[0]
+    # priors^^^
+    # Store
+    E_list.append(E_tot)
+    
+    # Also calulate bottom up, top down, and total prediction error (ie. L2 norm of the error vector) for each layer
+    PE_tot = np.sqrt(tot_err_sq)
+    PE_bu = np.sqrt(bu_err_sq)
+    PE_td = np.sqrt(td_err_sq)
+    # Store
+    PE_list.append((PE_tot, PE_bu, PE_td))
+    
+    return (E_tot, E_list, PE_list)
+
+
+
 
 '''
 inits
