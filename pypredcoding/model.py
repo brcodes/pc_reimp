a79cc24e14801e55f89a105cdbadfc1b9e51bc6f
Bryce <Rogers>
2024-08-16 21:30:42 -0400

    draft: r n1,2,3+ updates now have hard coded 1,2, and everything looks set.

--- pypredcoding/model.py
+++ pypredcoding/model.py
@@ -213,94 +213,77 @@ class PredictiveCodingClassifier:
         range_ndims_input = range(ndims_input)
         self.input_min_U1tdotr1_tdot_dims = list(range_ndims_input)
 
-        
     def r_updates_n_1(self, label):
         '''
-        will only work with static, like Us,
-        so when you set component_updates in recurrent, replace with entirely new updates
-        
-        # rn += kn/ssqn UnT F(rn-1 - f(Un rn)) (bottom up component)
-        # + C1/C2/None (top down component)
-        # - kn/2 g'(rn) (prior component)
-        
         move to static eventually, as well as update_Component assignment
         '''
-    
-        '''
-        
-        verify kn / 2 paradigm
-        '''
-
-        n = self.num_layers
             
-        kr_1 = self.kr[n]
-        ssq_1 = self.ssq[n]
-        U_1 = self.U[n]
-        r_1 = self.r[n]
+        kr_1 = self.kr[1]
+        ssq_1 = self.ssq[1]
+        U_1 = self.U[1]
+        r_1 = self.r[1]
         
         #U1 operations
         U1_transpose = np.transpose_U(U_1, self.U1_transpose_dims)
         U1_tdot_r1 = np.tensordot(U_1, r_1, axes=([-1],[0]))
-        input_min_U1tdotr1 = self.f(self.r[n-1] - self.f(U1_tdot_r1)[0])[1]
+        input_min_U1tdotr1 = self.f(self.r[0] - self.f(U1_tdot_r1)[0])[1]
         
-        self.r[n] += (kr_1 / ssq_1) * np.tensordot(U1_transpose, input_min_U1tdotr1, axes=(self.U1T_tdot_dims, self.input_min_U1tdotr1_tdot_dims)) + \
+        self.r[1] += (kr_1 / ssq_1) * np.tensordot(U1_transpose, input_min_U1tdotr1, axes=(self.U1T_tdot_dims, self.input_min_U1tdotr1_tdot_dims)) + \
                                                 + self.rn_topdown_cost_dict[self.classif_method](label) \
-                                                - (kr_1 / 2) * self.g(r_1, self.alph[n])[1]
+                                                - (kr_1 / 2) * self.g(r_1, self.alph[1])[1]
     
     def r_updates_n_2(self, label):
-            
-        n = self.num_layers
         
         '''
-        tensor dot and permutation
+        two layer model
         '''
-        #i
-        i = 1
-        kr_i = self.kr[i]
-        ssq_i = self.ssq[i]
-        r_i = self.r[i]
-        U_i = self.U[i]
-        #i=1
-        # ri += ki/ssqi UiT F(ri-1 - f(Ui ri)) (bottom up component)
-        # + ki+1/ssqi+1 (f(Ui+1 ri+1) - ri) (top down component)
-        # - ki/2 g'(ri) (prior component)
-        self.r[i] += (kr_i / ssq_i) * (U_i.T.dot(self.f(self.r[i-1] - self.f(U_i.dot(r_i))[0])[1])) + \
-                                            + (kr_n * ssq_n) * (self.f(U_n.dot(r_n))[0] - r_i) \
-                                            - (kr_i / 2) * self.g(r_i, self.alph[i])[1]
+        kr_1 = self.kr[1]
+        ssq_1 = self.ssq[1]
+        U_1 = self.U[1]
+        r_1 = self.r[1]
         
-        '''no permute and tensordot'''                             
-        #n
-        kr_n = self.kr[n]
-        ssq_n = self.ssq[n]
-        U_n = self.U[n]
-        r_n = self.r[n]
-        #n=2
-        # rn += kn/ssqn UnT F(rn-1 - f(Un rn)) (bottom up component)
-        # + C1/C2/None (top down component)
-        # - kn/2 g'(rn) (prior component)
-        self.r[n] += (kr_n / ssq_n) * (U_n.T.dot(self.f(r_i - self.f(U_n.dot(r_n))[0])[1])) + \
+        kr_2 = self.kr[2]
+        ssq_2 = self.ssq[2]
+        U_2 = self.U[2]
+        r_2 = self.r[2]
+        
+        #U1 operations
+        U1_transpose = np.transpose_U(U_1, self.U1_transpose_dims)
+        U1_tdot_r1 = np.tensordot(U_1, r_1, axes=([-1],[0]))
+        input_min_U1tdotr1 = self.f(self.r[0] - self.f(U1_tdot_r1)[0])[1]
+        
+        self.r[1] += (kr_1 / ssq_1) * np.tensordot(U1_transpose, input_min_U1tdotr1, axes=(self.U1T_tdot_dims, self.input_min_U1tdotr1_tdot_dims)) + \
+                                            + (kr_2 * ssq_2) * (self.f(U_2.dot(r_2))[0] - r_1) \
+                                            - (kr_1 / 2) * self.g(r_1, self.alph[1])[1]
+                                            
+        self.r[2] += (kr_2 / ssq_2) * (U_2.T.dot(self.f(self.r[1] - self.f(U_2.dot(r_2))[0])[1])) + \
                                                 + self.rn_topdown_cost_dict[self.classif_method](label) \
-                                                - (kr_n / 2) * self.g(r_n, self.alph[n])[1]
-                                                
+                                                - (kr_2 / 2) * self.g(r_2, self.alph[2])[1]
+                                            
     def r_updates_n_gt_eq_3(self, label):
-                                                
+        
         n = self.num_layers
+                                                
+        kr_1 = self.kr[1]
+        ssq_1 = self.ssq[1]
+        U_1 = self.U[1]
+        r_1 = self.r[1]
         
-        '''
-        premute and tensor dot
-        '''
+        kr_2 = self.kr[2]
+        ssq_2 = self.ssq[2]
+        U_2 = self.U[2]
+        r_2 = self.r[2]
         
-        #i
-        i = 1
-        kr_i = self.kr[i]
-        ssq_i = self.ssq[i]
-        r_i = self.r[i]
-        U_i = self.U[i]
-        self.r[i] += (kr_i / ssq_i) * (U_i.T.tensordot(self.f(self.r[i-1] - self.f(U_i.dot(r_i))[0])[1],)) + \
-                                                + (self.kr[i+1] * self.ssq[i+1]) * (self.f(self.U[i+1].dot(self.r[i+1]))[0] - r_i) \
-                                                - (kr_i / 2) * self.g(r_i, self.alph[i])[1]
+        #U1 operations
+        U1_transpose = np.transpose_U(U_1, self.U1_transpose_dims)
+        U1_tdot_r1 = np.tensordot(U_1, r_1, axes=([-1],[0]))
+        input_min_U1tdotr1 = self.f(self.r[0] - self.f(U1_tdot_r1)[0])[1]
         
-        ''' no permute and tensordot'''
+        # Layer 1
+        self.r[1] += (kr_1 / ssq_1) * np.tensordot(U1_transpose, input_min_U1tdotr1, axes=(self.U1T_tdot_dims, self.input_min_U1tdotr1_tdot_dims)) + \
+                                            + (kr_2 * ssq_2) * (self.f(U_2.dot(r_2))[0] - r_1) \
+                                            - (kr_1 / 2) * self.g(r_1, self.alph[1])[1]
+        # Layers 2 to n-1                                    
         for i in range(2,n):
             
             kr_i = self.kr[i]
@@ -308,23 +291,16 @@ class PredictiveCodingClassifier:
             r_i = self.r[i]
             U_i = self.U[i]
             
-            #i
-            # ri += ki/ssqi UiT F(ri-1 - f(Ui ri)) (bottom up component)
-            # + ki+1/ssqi+1 (f(Ui+1 ri+1) - ri) (top down component)
-            # - ki/2 g'(ri) (prior component)
             self.r[i] += (kr_i / ssq_i) * (U_i.T.dot(self.f(self.r[i-1] - self.f(U_i.dot(r_i))[0])[1])) + \
                                                 + (self.kr[i+1] * self.ssq[i+1]) * (self.f(self.U[i+1].dot(self.r[i+1]))[0] - r_i) \
                                                 - (kr_i / 2) * self.g(r_i, self.alph[i])[1]
     
-        #n
+        # Layer n
         kr_n = self.kr[n]
         ssq_n = self.ssq[n]
         U_n = self.U[n]
         r_n = self.r[n]
-        #n
-        # rn += kn/ssqn UnT F(rn-1 - f(Un rn)) (bottom up component)
-        # + C1/C2/None (top down component)
-        # - kn/2 g'(rn) (prior component)
+
         self.r[n] += (kr_n / ssq_n) * (U_n.T.dot(self.f(self.r[n-1] - self.f(U_n.dot(r_n))[0])[1])) + \
                                                 + self.rn_topdown_cost_dict[self.classif_method](label) \
                                                 - (kr_n / 2) * self.g(r_n, self.alph[n])[1]
